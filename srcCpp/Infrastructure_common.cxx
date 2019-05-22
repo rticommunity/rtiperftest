d77525ea2a549ef1c260c25f49ddf9cbf2775a28
Salva Corts <salvacorts97@gmail.com>
2019-05-22 10:44:55 +0200

    chrono was not supported in C++98
    Now we are using time.h in VxWorks testing

--- srcCpp/Infrastructure_common.cxx
+++ srcCpp/Infrastructure_common.cxx
@@ -5,22 +5,23 @@
 
 #include "Infrastructure_common.h"
 
-#include <chrono>
+#include <sys/time.h>
 
 void *PerftestTimer::waitAndExecuteHandler(void *scheduleInfo) 
 {
     ScheduleInfo *info = static_cast<ScheduleInfo *>(scheduleInfo);
 
-    using namespace std::chrono;
+    struct timeval t1, t2;
+    double elapsedTime;
     printf("[++++++++++] Voy a dormir %u ms\n", info->timer);
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    gettimeofday(&t1, NULL);
 
     // Sleep for t milliseconds
     PerftestClock::milliSleep(info->timer * 1000u);
 
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-    printf("[----------] Ya he dormido %f s\n", time_span.count());
+    gettimeofday(&t2, NULL);
+    elapsedTime = (t2.tv_sec - t1.tv_sec);
+    printf("[----------] Ya he dormido %f s\n", elapsedTime);
 
     // Call the scheduled function with the args
     info->handlerFunction();
