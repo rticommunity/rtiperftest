b8287a7ea4c6ebe0bb0ac4ddc3c3073790125dc0
Salva Corts <salvacorts97@gmail.com>
2019-05-24 11:32:06 +0200

    Remove debug info

--- srcCpp/perftest_publisher.cxx
+++ srcCpp/perftest_publisher.cxx
@@ -77,8 +77,6 @@ int perftest_cpp::Run(int argc, char *argv[])
 {
     PrintVersion();
 
-    printf("[#####] PM addr: %p\n", &_PM);
-
     try {
         _PM.initialize();
     } catch(std::exception &ex) {
@@ -663,8 +661,6 @@ class ThroughputListener : public IMessagingCB
             }
 
             begin_time = PerftestClock::getInstance().getTimeUsec();
-
-            printf("[#####] printIntervals addr: %p\n", &printIntervals);
 
             if (printIntervals) {
                 printf("\n\n********** New data length is %d\n",
