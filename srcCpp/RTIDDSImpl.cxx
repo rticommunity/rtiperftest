a7b4ac409d2a606dee41f8516288d9ddb8611814
Salva Corts <salvacorts97@gmail.com>
2019-08-05 08:37:21 +0200

    Take +1 sample into account

--- srcCpp/RTIDDSImpl.cxx
+++ srcCpp/RTIDDSImpl.cxx
@@ -2804,8 +2804,9 @@ DDS_ReturnCode_t RTIDDSImpl<T>::setup_DW_QoS(DDS_DataWriterQos &dw_qos, std::str
                 }
             }
           #endif
+            // The writer_loaned_sample_allocation is initial_simples + 1
             initial_samples = std::max(
-                    1, max_allocable_space / RTI_FLATDATA_MAX_SIZE);
+                    1, (max_allocable_space - RTI_FLATDATA_MAX_SIZE) / RTI_FLATDATA_MAX_SIZE);
 
             initial_samples = std::min(
                     initial_samples,
