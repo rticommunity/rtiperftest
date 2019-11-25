9b6b51ccab9e98764cbc112b7147c40b69b7777e
Javier Morales <jmorales@rti.com>
2019-11-25 16:17:40 +0100

    WIP

--- srcCpp/Infrastructure_pro.cxx
+++ srcCpp/Infrastructure_pro.cxx
@@ -11,18 +11,6 @@
 
 #include <time.h>
 using namespace std;
-/*
- * Since std::to_string is not defined until c++11
- * we will define it here.
- */
-namespace std {
-    template<typename T>
-    std::string to_string(const T &n) {
-        std::ostringstream s;
-        s << n;
-        return s.str();
-    }
-}
 
 /* Perftest Clock class */
 
