a584c04f5b7b7acd180742db274673398a1a224f
Javier Morales <jmorales@rti.com>
2019-12-04 12:23:55 +0100

    Add send queue size dependent qos also to latency topic

--- srcCpp/RTIDDSImpl.cxx
+++ srcCpp/RTIDDSImpl.cxx
@@ -3008,16 +3008,50 @@ bool RTIDDSImpl<T>::setup_DW_QoS(
       #endif
     }
 
-    if (qos_profile == "LatencyQos"
-            && _PM->get<bool>("noDirectCommunication")
-            && (_PM->get<int>("durability") == DDS_TRANSIENT_DURABILITY_QOS
-            || _PM->get<int>("durability") == DDS_PERSISTENT_DURABILITY_QOS)) {
-        dw_qos.durability.kind =
-                (DDS_DurabilityQosPolicyKind)_PM->get<int>("durability");
+    if (qos_profile == "LatencyQos") {
+
+        if (_PM->get<bool>("noDirectCommunication")
+                && (_PM->get<int>("durability") == DDS_TRANSIENT_DURABILITY_QOS
+                || _PM->get<int>("durability") == DDS_PERSISTENT_DURABILITY_QOS)) {
+            dw_qos.durability.kind =
+                    (DDS_DurabilityQosPolicyKind)_PM->get<int>("durability");
+          #ifndef RTI_MICRO
+            dw_qos.durability.direct_communication =
+                    !_PM->get<bool>("noDirectCommunication");
+          #endif
+        }
+
+
+        dw_qos.protocol.rtps_reliable_writer.heartbeats_per_max_samples =
+                _PM->get<int>("sendQueueSize") / 10;
       #ifndef RTI_MICRO
-        dw_qos.durability.direct_communication =
-                !_PM->get<bool>("noDirectCommunication");
+        dw_qos.protocol.rtps_reliable_writer.low_watermark =
+                _PM->get<int>("sendQueueSize") * 1 / 10;
+        dw_qos.protocol.rtps_reliable_writer.high_watermark =
+                _PM->get<int>("sendQueueSize") * 9 / 10;
+
+        /*
+         * If _SendQueueSize is 1 low watermark and high watermark would both be
+         * 0, which would cause the middleware to fail. So instead we set the
+         * high watermark to the low watermark + 1 in such case.
+         */
+        if (dw_qos.protocol.rtps_reliable_writer.high_watermark
+                == dw_qos.protocol.rtps_reliable_writer.low_watermark) {
+            dw_qos.protocol.rtps_reliable_writer.high_watermark =
+                    dw_qos.protocol.rtps_reliable_writer.low_watermark + 1;
+        }
+
+        dw_qos.protocol.rtps_reliable_writer.max_send_window_size =
+                _PM->get<int>("sendQueueSize");
+        dw_qos.protocol.rtps_reliable_writer.min_send_window_size =
+                _PM->get<int>("sendQueueSize");
+      #else
+        dw_qos.history.depth = _PM->get<int>("sendQueueSize");
+        // Same values we use for Pro (See perftest_qos_profiles.xml).
+        dw_qos.protocol.rtps_reliable_writer.heartbeat_period.sec = 0;
+        dw_qos.protocol.rtps_reliable_writer.heartbeat_period.nanosec = 10000000;
       #endif
+
     }
 
     dw_qos.resource_limits.max_instances =
