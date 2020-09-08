6641a9054ca858166995c0da55304f9e5d379eb4
Javier Morales <jmorales@rti.com>
2020-09-08 04:21:52 -0400

    Fix RTI_MICRO

--- srcCpp/RTIDDSImpl.cxx
+++ srcCpp/RTIDDSImpl.cxx
@@ -3170,7 +3170,7 @@ bool RTIDDSImpl<T>::setup_DW_QoS(
           #endif
         }
 
-      #ifndef RTI_MICRO
+      #ifndef PERTEST_RTI_MICRO
 
         if (_PM->get<unsigned long long>("dataLen") > DEFAULT_MESSAGE_SIZE_MAX) {
             dw_qos.protocol.rtps_reliable_writer.heartbeats_per_max_samples =
